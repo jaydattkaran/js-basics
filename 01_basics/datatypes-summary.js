d1b8976a15a2ca64c76fb5103547a2fdc4ade200
Jaydatt Karan <124327565+jaydattkaran@users.noreply.github.com>
2024-02-01 13:29:00 +0000

    feat: Add JavaScript basics and control flow examples
    Added files:
    - 01_basics/01_variables.js
    - 01_basics/02_dataTypes.js
    - 01_basics/03_conversionOperation.js
    - 01_basics/04_comparison.js
    - 01_basics/05_strings.js
    - 01_basics/06_nums_and_maths.js
    - 01_basics/07_datesinJs.js
    - 01_basics/datatypes-summary.js
    - 02_basics/01_arrays.js
    - 02_basics/02_array.js
    - 02_basics/03_objects.js
    - 02_basics/04_objects.js
    - 03_basics/01_function.js
    - 03_basics/02_scopes.js
    - 03_basics/03_arrow.js
    - 03_basics/04_iife.js
    - 04_control_flow/one.js
    - 04_control_flow/switch.js
    - 04_control_flow/truthy.js
    - 05_iterations/five.js
    - 05_iterations/four.js
    - 05_iterations/nine.js
    - 05_iterations/one.js
    - 05_iterations/seven.js
    - 05_iterations/six.js
    - 05_iterations/three.js
    - 05_iterations/two.js
    - 06_dom/one.html
    
    Removed files:
    - 01_basics/test.js

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,55 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// console.log(typeof heros);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYouTubename = "thekaran"
+let anothername = myYouTubename
+console.log(anothername);
+
+let userOne = {
+    email: "jay@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "jay@mail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
